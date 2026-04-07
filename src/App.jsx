c2cb2f46c7319d44862e93d1f3ec952aab8ebf88
RazikKhan <mdrazikkhan779@gmail.com>
2026-04-07 13:29:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-amber-600 text-6xl text-center mt-20">
+        Welcome to React
+      </h1>
+    </>
+  );
+}
+
+export default App;
